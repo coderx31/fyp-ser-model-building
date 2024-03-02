cb5a6d3845ac438980ff59ce503c783c31548309
= <=>
2024-03-02 22:04:22 +0530

    audion features saved to new csv file

---
+++ main.py
@@ -0,0 +1,9 @@
+# model testing
+import pandas as pd
+
+# removed labels columns and saved to another file
+# df = pd.read_csv("/Users/shenalfernando/Documents/projects/fyp-ser-model-implementation/notebooks/tess_evaluation_features.csv")
+# df = df.drop(columns=['labels'])
+# print(df.head())
+# print(df.iloc[0])
+# df.to_csv("tess_features.csv")
